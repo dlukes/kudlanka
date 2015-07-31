fe8ce277b41cab2d9070db7e7e8d36f0a84a2884
David Lukes <dafydd.lukes@gmail.com>
2015-07-31 23:17:02 +0200

    implement GET seg API

--- kudlanka/__init__.py
+++ kudlanka/__init__.py
@@ -3,6 +3,7 @@ from flask.ext.mongoengine import MongoEngine
 from flask.ext.security import Security, MongoEngineUserDatastore, RoleMixin, \
     UserMixin, login_required
 from flask.ext.security.forms import LoginForm
+from flask.ext.restful import Resource, Api
 
 from wtforms import TextField, PasswordField, SubmitField, BooleanField
 
@@ -19,6 +20,8 @@ app.config["MONGODB_PORT"] = 27017
 
 db = MongoEngine(app)
 
+## User management
+
 
 class Role(db.Document, RoleMixin):
     name = db.StringField(max_length = 80, unique = True)
@@ -30,6 +33,35 @@ class User(db.Document, UserMixin):
     password = db.StringField(max_length = 255)
     active = db.BooleanField(default = True)
     roles = db.ListField(db.ReferenceField(Role), default = [])
+    assigned = db.StringField()
+    segs = db.ListField(db.StringField())
+
+
+## Data storage
+
+
+class Seg(db.Document):         # DynamicDocument
+    meta = {
+        "collection": "segs",
+        "indexes": [
+            {"fields": ["sid"], "unique": True},
+            {"fields": ["done", "assigned", "users"]}
+        ]
+    }
+    sid = db.StringField(max_length = 10, required = True)
+    oral = db.StringField(max_length = 10, required = True)
+    num = db.StringField(max_length = 10, required = True)
+    done = db.IntField(required = True)
+    assigned = db.BooleanField(required = True)
+    users = db.ListField(required = True)
+    utt = db.ListField(required = True)
+
+
+segs = []
+@app.before_first_request
+def retrieve_unprocessed_segs():
+    for seg in Seg.objects():
+        segs.append(seg.sid)
 
 
 # Flask-Security setup
@@ -47,6 +79,8 @@ security = Security(app, user_datastore, login_form = KudlankaLogin)
 
 # Routes
 
+## UI
+
 
 @app.route("/")
 @login_required
@@ -60,7 +94,28 @@ def list():
     return "Seznam promluv"
 
 
-@app.route("/edit/<id>")
+@app.route("/edit")
 @login_required
-def edit(id):
-    return render_template("edit.html", id = id)
+def edit():
+    return render_template("edit.html")
+
+
+## API
+
+api = Api(app, decorators = [login_required])
+
+
+class SegApi(Resource):
+    def get(self, sid):
+        if sid == "random":
+            seg = Seg.objects(done = 0,
+                              assigned = False,
+                              users__nin = session["user_id"]).first()
+        else:
+            seg = Seg.objects(sid = sid).first()
+        seg = seg.to_mongo()
+        seg["_id"] = str(seg["_id"])
+        return seg
+
+
+api.add_resource(SegApi, "/api/seg/<string:sid>")
