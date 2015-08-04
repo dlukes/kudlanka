2d1eed3c0e09060f3e3fae02e76da8e3e09fb221
David Lukes <dafydd.lukes@gmail.com>
2015-08-04 17:35:18 +0200

    list available URL routes when testing

--- runserver.py
+++ runserver.py
@@ -1,5 +1,18 @@
 from kudlanka import app
+from flask import url_for
 
-app.run(debug = True, host = "::")
+def has_no_empty_params(rule):
+    defaults = rule.defaults if rule.defaults is not None else ()
+    arguments = rule.arguments if rule.arguments is not None else ()
+    return len(defaults) >= len(arguments)
+
+if app.config["SERVER_NAME"]:
+    print("Listing available routes...")
+    for rule in app.url_map.iter_rules():
+        if "GET" in rule.methods and has_no_empty_params(rule):
+            with app.app_context():
+                print(url_for(rule.endpoint))
+
+app.run(debug = True, host = "localhost", port = 1993)
 
 # the app is then accessible on the LAN via http://trnka:5000
