902b24885a328474ecd7d65460be9da070b7ba1f
David Lukes <dafydd.lukes@gmail.com>
2015-08-05 17:33:21 +0200

    add Czech translation for data table UI

--- kudlanka/static/js/list.js
+++ kudlanka/static/js/list.js
@@ -1,5 +1,22 @@
 $(document).ready(function () {
   $("#seg-table").DataTable({
-    order: []
+    order: [],
+    language: {
+      "sProcessing":   "Provádím...",
+      "sLengthMenu":   "Zobraz záznamů _MENU_",
+      "sZeroRecords":  "Žádné záznamy nebyly nalezeny",
+      "sInfo":         "Zobrazuji _START_ až _END_ z celkem _TOTAL_ záznamů",
+      "sInfoEmpty":    "Žádné záznamy k zobrazení",
+      "sInfoFiltered": "(filtrováno z celkem _MAX_ záznamů)",
+      "sInfoPostFix":  "",
+      "sSearch":       "Hledat:",
+      "sUrl":          "",
+      "oPaginate": {
+        "sFirst":    "První",
+        "sPrevious": "Předchozí",
+        "sNext":     "Další",
+        "sLast":     "Poslední"
+      }
+    }
   });
 });
