418268987eb92285664dfc6b4acacf074724444c
David Lukes <dafydd.lukes@gmail.com>
2015-09-22 10:26:40 +0200

    add html export

--- utils/export.py
+++ utils/export.py
@@ -1,25 +1,31 @@
 #!/usr/bin/env python3
 
-"""Export all corpus positions that have been disambiguated at least once.
+"""Summarize results of manual disambiguation using Kudlanka.
 
-default: as a simple tab-separated file.
-
-Usage: run w/o arguments; output goes to STDOUT.
+Output: either as a single csv file, or as a set of linked html files.
 
 """
 
+from pprint import pprint
 import os
 import sys
 import argparse
 import logging
 
-SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+# SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+SCRIPT_DIR = os.path.dirname("/home/lukes/src/kudlanka/utils/")
 sys.path.append(os.path.join(SCRIPT_DIR, ".."))
 
 from collections import defaultdict
+from functools import lru_cache
 from mongoengine import connect
 from kudlanka.config import MONGODB_DB
-from kudlanka.models import Seg
+from kudlanka.models import Seg, User
+
+
+@lru_cache(maxsize=None)
+def uid2username(id):
+    return User.objects(id=id).first().email
 
 
 def csv_output():
@@ -30,13 +36,102 @@ def csv_output():
     for seg in Seg.objects(users_size__gt=0):
         for idx, pos in enumerate(seg.utt):
             by_user = defaultdict(dict)
+            word = pos.get("word", None)
             for field in ["lemmas", "tags", "flags", "notes"]:
                 for uid, value in pos.get(field, {}).items():
                     by_user[uid][field[:-1]] = value
+                    by_user[uid]["word"] = word
             for uid, fields in by_user.items():
-                out.writerow([uid, seg.sid, idx, fields.get("lemma", None),
-                            fields.get("tag", None), fields.get("flag", None),
-                            fields.get("note", None)])
+                out.writerow([uid2username(uid), seg.sid, idx,
+                              fields.get("lemma", None), fields.get("tag", None),
+                              fields.get("flag", None), fields.get("note", None)])
+
+
+def html_output(outdir):
+    by_word = defaultdict(dict)
+    for seg in Seg.objects(users_size__gt=0):
+        utt = [pos["word"] for pos in seg.utt]
+        for idx, pos in enumerate(seg.utt):
+            lemmas = set([lemma for lemma in pos.get("lemmas", {}).values()])
+            tags = set([tag for tag in pos.get("tags", {}).values()])
+            flags = [1 for flag in pos.get("flags", {}).values() if flag]
+            notes = [note for note in pos.get("notes", {}).values()]
+            # - 2 because there will always be at least one lemma and tag
+            pos_cont = len(lemmas) + len(tags) + len(flags) + len(notes) - 2
+            if pos_cont > 0:
+                by_user = defaultdict(dict)
+                word = pos["word"]
+                for field in ["lemmas", "tags", "flags", "notes"]:
+                    for uid, value in pos.get(field, {}).items():
+                        by_user[uid2username(uid)][field[:-1]] = value
+                by_word[word].setdefault("cont", 0)
+                by_word[word]["cont"] += pos_cont
+                by_word[word].setdefault("inst", [])
+                by_word[word]["inst"].append(
+                    dict(sid=seg.sid, idx=idx, utt=utt, annot=by_user))
+    render_html(by_word, outdir)
+
+
+def render_html(by_word, outdir):
+    from jinja2 import Template
+    from sortedcontainers import SortedListWithKey
+    cont_html = Template("""
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+</head>
+<body>
+<ul>
+{% for word in by_cont %}
+    <li><a href="{{ word.word }}__.html">{{ word.word }}</a>: {{ word.cont }}</li>
+{% endfor %}
+</ul>
+</body>
+</html>
+""")
+    word_html = Template("""
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+</head>
+<body>
+{% for inst in word.inst %}
+<h3><a href="https://trnka.korpus.cz/kudlanka/edit/{{ inst.sid }}">Věta</a></h3>
+<p>
+    {% for pos in inst.utt %}
+    {% if loop.index - 1 == inst.idx %}
+    <span style="color: red">{{ pos }}</span>
+    {% else %}
+    {{ pos }}
+    {% endif %}
+    {% endfor %}
+</p>
+<h3>Anotace</h3>
+    {% for user, annots in inst.annot.items() %}
+    <h4>{{ user }}</h4>
+    <dl>
+    {% for type, annot in annots.items() %}
+    <dt>{{ type }}</dt>
+    <dd>{{ annot }}</dd>
+    {% endfor %}
+    </dl>
+    {% endfor %}
+    <hr>
+{% endfor %}
+</body>
+</html>
+""")
+    by_cont = SortedListWithKey(key=lambda dic: -dic["cont"])
+    for word, dic in by_word.items():
+        dic["word"] = word
+        by_cont.add(dic)
+    with open(os.path.join(outdir, "index.html"), "w") as fh:
+        print(cont_html.render(by_cont=by_cont), file=fh)
+    for word in by_cont:
+        with open(os.path.join(outdir, word["word"] + "__.html"), "w") as fh:
+            print(word_html.render(word=word), file=fh)
 
 
 def parse_arguments(argv):
@@ -50,7 +145,7 @@ def parse_arguments(argv):
         description="Export all corpus positions that have been disambiguated "
         "at least once")
     # parser.add_argument("input_file", nargs="+")
-    # parser.add_argument("-o", "--output", help="output file")
+    parser.add_argument("-o", "--outdir", help="output directory", default=".")
     parser.add_argument("-l", "--log", help="logging verbosity",
                         default="DEBUG",
                         choices=["DEBUG", "INFO", "WARNING", "ERROR"])
@@ -74,6 +169,8 @@ def main(argv=None):
     connect(MONGODB_DB)
     if args.format == "csv":
         csv_output()
+    elif args.format == "html":
+        html_output(args.outdir)
     return 0
 
 
